b444b8efaf928ab217dcb4a25e69cc63222b6c74
deadofarceus <nikilanger@yahoo.de>
2023-05-11 13:16:52 +0200

    fertig Clueless

--- scripts/bingo.js
+++ scripts/bingo.js
@@ -3,42 +3,41 @@ const prompts = [
     "Frank verwendet den Ausdruck \"Geschmacksexplosion\"",
     "Frank kritisiert die Sauberkeit in der K\u00FCche",
     "Frank spricht \u00FCber die Bedeutung von frischen Zutaten",
-    "Frank stellt fest, dass das Restaurant zu viele Gerichte auf der Speisekarte hat",
-    "Frank gibt dem Restaurantbesitzer eine ehrliche, aber konstruktive Kritik",
+    "Zu viele Gerichte auf der Speisekarte",
+    "Frank gibt tats\u00E4chlich konstruktive Kritik",
     "Frank spricht \u00FCber die Bedeutung von gutem Kundenservice",
-    "Der Restaurantbesitzer weint, wenn Frank mit ihm spricht",
+    "Der Besitzer weint, wenn Frank mit ihm spricht",
     "Frank entdeckt ein veraltetes oder unhygienisches K\u00FCchenger\u00E4t",
     "Das Restaurant hat einen originellen oder ungew\u00F6hnlichen Namen",
     "Frank spricht \u00FCber die Wichtigkeit einer klaren Zielgruppe f\u00FCr das Restaurant",
-    "Der Restaurantbesitzer erkl\u00E4rt, dass er/sie das Restaurant aus Leidenschaft betreibt",
-    "Frank spricht \u00FCber die Bedeutung von Marketing und Social Media f\u00FCr das Restaurant",
+    "Der Restaurantbesitzer betreibt das Restaurant aus voller Leidenschaft",
+    "Frank spricht \u00FCber die Bedeutung von Marketing f\u00FCr das Restaurant",
     "Ein Testesser findet das Essen ungenie\u00DFbar oder spuckt es aus",
     "Ein Testesser kritisiert die Portionengr\u00F6\u00DFe",
     "Frank zwinkert (kutcherGeil)",
     "Frank macht einen unglaublich unangenehmen Witz",
     "Frank macht unangenehme Ann\u00E4herungen einer Frau gegen\u00FCber",
-    "Ein Testesser \u00E4u\u00DFert Bedenken hinsichtlich der Auswahl f\u00FCr vegetarische oder vegane G\u00E4ste",
+    "Ein Testesser beschwert sich \u00FCber die Auswahl f\u00FCr vegetarische oder vegane G\u00E4ste",
     "Ein Testesser beschreibt ein Gericht als \"Geschmacksexplosion\", genau wie Frank es tun w\u00FCrde",
-    "Frank macht einen \u00FCberraschten oder am\u00FCsierten Gesichtsausdruck aufgrund eines unerwarteten Kommentars eines Testessers",
-    "Ein Testesser erw\u00E4hnt, dass er die Sendung \"Rosins Restaurants\" regelm\u00E4\u00DFig verfolgt",
-    "Frank teilt eine Anekdote oder eine lustige Geschichte aus einer vergangenen Episode der Sendung",
+    "Frank macht einen \u00FCberraschten Gesichtsausdruck aufgrund eines Kommentars eines Testessers",
+    "Ein Testesser erw\u00E4hnt, dass er die Sendung \"Rosins Restaurants\" verfolgt",
+    "Frank teilt eine Anekdote aus einer vergangenen Episode der Sendung",
     "MEGA",
-    "Frank macht einen provokativen Kommentar \u00FCber die Kochk\u00FCnste des Restaurantbesitzers",
+    "Frank macht einen provokativen Kommentar \u00FCber die Kochk\u00FCnste des Besitzers",
     "MEGA b2b",
     "MEGA b3b",
-    "Der Restaurantbesitzer reagiert ver\u00E4rgert auf Franks harte Kritik und ger\u00E4t in eine hitzige Diskussion",
-    "Frank fordert den Restaurantbesitzer auf, pers\u00F6nliche Probleme offenzulegen, was f\u00FCr Unbehagen sorgt",
-    "Frank unterbricht den Restaurantbesitzer immer wieder, wenn dieser versucht, sich zu verteidigen",
+    "Der Besitzer reagiert ver\u00E4rgert auf Franks harte Kritik und ger\u00E4t in eine hitzige Diskussion",
+    "Frank fordert den Besitzer auf, pers\u00F6nliche Probleme offenzulegen, was f\u00FCr Unbehagen sorgt",
+    "Frank unterbricht den Besitzer, wenn dieser versucht, sich zu verteidigen",
     "Frank organisiert einen Foodtruck",
     "Bewertung erreicht 5 Sterne oder \u00FCber 45 Punkte",
-    "Frank konfrontiert den Restaurantbesitzer mit seinen schlechten finanziellen Entscheidungen vor den Augen der Mitarbeiter",
-    "Der Restaurantbesitzer versucht, Franks unangenehmen Bemerkungen mit Humor zu begegnen, aber es ist offensichtlich, dass er/sie verletzt ist",
-    "Frank provoziert den Restaurantbesitzer, indem er seine F\u00FChrungsqualit\u00E4ten in Frage stellt",
+    "Frank konfrontiert den Besitzer mit seinen schlechten finanziellen Entscheidungen vor den Augen der Mitarbeiter",
+    "Frank provoziert den Besitzer, indem er seine F\u00FChrungsqualit\u00E4ten in Frage stellt",
     "Franks Team hat eine geheime Kamera\u00FCberwachung verwendet und ist stolz darauf.",
     "Der Besitzer merkt, dass Frank nur f\u00FCr den Content da ist und nicht um sein Restaurant zu retten",
     "Die Musik im Hintergrund \u00FCbert\u00F6nt die Dialoge der Personen und macht sie schwer verst\u00E4ndlich",
-    "Die Musik wechselt in 10 Sekunden viel zu h\u00E4ufig",
-    "W\u00E4hrend einer Besprechung zwischen Frank und dem Restaurantbesitzer \u00E4ndert sich pl\u00F6tzlich die Anordnung der Gegenst\u00E4nde auf dem Tisch",
+    "Die Musik wechselt in wenigen Sekunden viel zu h\u00E4ufig",
+    "W\u00E4hrend einer Besprechung zwischen Frank und dem Besitzer \u00E4ndert sich pl\u00F6tzlich das Szenenbild",
 ];
 // Retrieve the matrix from localStorage
 var matrix = JSON.parse(localStorage.getItem("bingoMatrix"));
@@ -109,24 +108,28 @@ function toggleBackgroundColor(event) {
     }
 }
 
-// Function to adjust the font size dynamically based on the text content
 function adjustFontSize() {
     var textareas = document.getElementsByTagName("textarea");
     for (var i = 0; i < textareas.length; i++) {
         var textarea = textareas[i];
         textarea.style.fontSize = "25px"; // Reset font size
     
-        // Calculate the height required to fit the content without scrolling
-        var scrollHeight = textarea.scrollHeight;
-        var clientHeight = textarea.clientHeight;
-        var heightDiff = scrollHeight - clientHeight;
-    
-        // Adjust the font size based on the height difference
-        if (heightDiff > 0) {
-            var fontSize = parseInt(window.getComputedStyle(textarea).fontSize);
-            var newFontSize = fontSize - heightDiff / 17; // Adjust the division factor as needed
-            textarea.style.fontSize = newFontSize + "px";
-        }
+        var contentHeight = textarea.scrollHeight;
+        var textareaHeight = textarea.clientHeight;
+        
+        // Calculate the ratio of content height to textarea height
+        var ratio = contentHeight / textareaHeight;
+        
+        // Adjust the font size based on the ratio
+        var fontSize = 25; // Initial font size
+        var maxFontSize = 40; // Maximum font size
+        var minFontSize = 14; // Minimum font size
+        
+        // Calculate the new font size within the specified range
+        var newFontSize = Math.max(minFontSize, Math.min(maxFontSize, fontSize / ratio));
+        
+        textarea.style.fontSize = newFontSize + "px";
+        textarea.style.height = (650 - ((matrix.length + 1) * 5))/matrix.length + "px";
     }
 }
   
