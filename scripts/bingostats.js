fb629d91828fb18bf2cc8bd6d8b54c27688380bc
deadofarceus <nikilanger@yahoo.de>
2023-08-12 16:39:40 +0200

    getBingoLeaderBoard

--- scripts/bingostats.js
+++ scripts/bingostats.js
@@ -1,18 +1,39 @@
-const baseUrl = 'https://crystal-reliable-slipper.glitch.me';
+var socket = new WebSocket(`wss://crystal-reliable-slipper.glitch.me`);
 
-// Hier passt du den Pfad an, um die gewünschten Endpunkte auf deinem Server zu erreichen
-const dataUrl = `${baseUrl}/data`;
+function connectWebSocket() {
+    socket = new WebSocket(`wss://crystal-reliable-slipper.glitch.me`);
+  
+    socket.onopen = function() {
+      console.log('WebSocket-Verbindung hergestellt.');
+      sendMessage();
+    };
 
-// fetch(dataUrl)
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log('Empfangene Daten:', data);
-//         // Hier kannst du mit den empfangenen Daten arbeiten
-//     })
-//     .catch(error => {
-//         console.error('Fehler bei der Anfrage:', error);
-//     });
-fetch(dataUrl, { mode: 'no-cors' })
-    .catch(error => {
-        console.error('Fehler bei der Anfrage:', error);
-    });
+    socket.onclose = function() {
+      console.log('WebSocket-Verbindung geschlossen. Versuche erneut zu verbinden...');
+      setTimeout(connectWebSocket, 2000); // Verbindung nach 2 Sekunden erneut aufbauen
+    };
+  
+    socket.onerror = function(error) {
+      console.error('WebSocket-Fehler aufgetreten: ', error);
+    };
+
+    socket.onmessage = function (event) {
+        const data = event.data;
+
+        showData(JSON.parse(data)); // ARRAY in form von [["deadofarceus",1]]
+
+        console.log('Nachricht vom Server erhalten:', data);
+    };
+}
+
+// Initialer Verbindungsaufbau
+connectWebSocket();
+
+function sendMessage() {
+    socket.send("GET BINGO DATA");
+    console.log('Request sent');
+}
+
+function showData(correctBingos) {
+    //TODO zeig einfach die map als Leaderboard sollte nicht os schwer sein Clueless
+}
