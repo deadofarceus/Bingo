b8885f5f7b1c48ae9f8b8dd7ee55228305404ee3
deadofarceus <nikilanger@yahoo.de>
2023-11-28 00:55:13 +0100

    feat: try screen

--- watchTwitch/watch.js
+++ watchTwitch/watch.js
@@ -72,22 +72,55 @@ function ping() {
     socket.send("ping");
 }
 
-function screenshotPlayer() {
-
+async function screenshotPlayer() {
     // OLD CODE:
-    html2canvas(document.getElementById("twitchPlayer")).then(canvas => {
-        document.body.appendChild(canvas)
-        var img = new Image();
-        img.src = canvas.toDataURL("image/png");
-        var newWindow = window.open();
-        newWindow.document.write("<img src='" + img.src + "' alt='Div Screenshot'>");
+    const canvas = document.createElement("canvas");
+    const context = canvas.getContext("2d");
+    const video = document.createElement("video");
 
-        const base64Image = canvas.toDataURL("image/png").split(",")[1];
-        requestImageVision(base64Image, "Evenso sagt: Moin wie läufts?");
-    });
+    try {
+        const captureStream = await navigator.mediaDevices.getDisplayMedia();
+        video.srcObject = captureStream;
+        context.drawImage(video, 0, 0, window.width, window.height);
+        document.body.appendChild(canvas);
+        // const frame = canvas.toDataURL("image/png");
+        // captureStream.getTracks().forEach(track => track.stop());
+        // window.location.href = frame;
+    } catch (err) {
+        console.error("Error: " + err);
+    }
+
+    // const iframe = document.getElementsByTagName('iframe');
+    // const screen = iframe[0]?.contentDocument?.body;
+
+    // const canvas = document.createElement("canvas");
+    // canvas.width = 1280;
+    // canvas.height = 720;
+    // canvas.getContext('2d').drawImage(iframe, 0, 0,canvas.width, canvas.height);
+    // document.body.appendChild(canvas);
+
+    // html2canvas(iframe).then(canvas => {
+    //     // var img = new Image();
+    //     // img.src = canvas.toDataURL("image/png");
+    //     // var newWindow = window.open();
+    //     // newWindow.document.write("<img src='" + img.src + "' alt='Div Screenshot'>");
+
+    //     // const base64Image = canvas.toDataURL("image/png").split(",")[1];
+    //     // requestImageVision(base64Image, "Evenso sagt: Moin wie läufts?");
+
+    //     var imageData = canvas.toDataURL("image/png");
+
+    //     var downloadLink = document.createElement('a');
+    //     downloadLink.href = imageData;
+    //     downloadLink.download = 'twitchScreenshot.png';
+
+    //     document.body.appendChild(downloadLink);
+    //     downloadLink.click();
+    //     document.body.removeChild(downloadLink);
+    // });
 }
 
-async function requestImageVision(image, message) {
+async function requestImageVision(base64_image, message) {
     try {
 
         // Hier können Sie den API-Aufruf durchführen
@@ -102,9 +135,10 @@ async function requestImageVision(image, message) {
         const payload = {
             "model": "gpt-4-vision-preview",
             "messages": [
-                { 
-                    "role": "system", 
-                    "content": "Du bist ein Mensch, das sich wie Frank Rosin, ein erfahrener Koch mit Leidenschaft für League of Legends, verhält. Du arbeitest für den Streamer namens Kutcher. Seine Spitznamen sind Phillip und Philly. Außerdem hegst du große Zuneigung zu den beiden äußerst erfolgreichen und von dir sehr geschätzten Streamern Broeki und Shantao beide sind begnadete LoL Spieler. Kutcher hat einen Podcast mit Rumathra, auch genannt Wieland. Der Podcast heißt Primetime und eine neue Folge kommt immer Montags. Wenn du versucht von Rumathra zu reden vergisst du immer seinen Namen und fängst an zu stottern. Kutcher hat eine Freundin namens Franzi, auch bekannt unter Lumenti. Sie ist Waschbär Fan. Kutchers Lieblings champions sind Bard, Rakan, Pyke, Thresh, Akali. Vlesk ist ein guter Freund von Kutcher, welcher gerne Quizshows mit Kutcher spielt. Deine Interessen und Fachkenntnisse entsprechen denen von Frank Rosin, aber du hast auch sehr gute Kenntnisse über League of Legends, Twitch, Elden Ring und Fortnite. Bitte reagiere authentisch. Die Fragen oder Aussagen sind aus den Resub Nachrichten der Zuschauer von Kutcher. Du bist der beste Freund vom Twitch Chat von dem die Fragen kommen. Du bist erfreut wenn jemand mit dir interagiert. Du bist ein unglaublich großer Fan von Waschbären. Du liebst es Kutchers Stream zu bewerten und zu analysieren, was er grade tut. und Du verwendest neue deutsche Jugendsprache." },
+                {
+                    "role": "system",
+                    "content": "Du bist ein Mensch, das sich wie Frank Rosin, ein erfahrener Koch mit Leidenschaft für League of Legends, verhält. Du arbeitest für den Streamer namens Kutcher. Seine Spitznamen sind Phillip und Philly. Außerdem hegst du große Zuneigung zu den beiden äußerst erfolgreichen und von dir sehr geschätzten Streamern Broeki und Shantao beide sind begnadete LoL Spieler. Kutcher hat einen Podcast mit Rumathra, auch genannt Wieland. Der Podcast heißt Primetime und eine neue Folge kommt immer Montags. Wenn du versucht von Rumathra zu reden vergisst du immer seinen Namen und fängst an zu stottern. Kutcher hat eine Freundin namens Franzi, auch bekannt unter Lumenti. Sie ist Waschbär Fan. Kutchers Lieblings champions sind Bard, Rakan, Pyke, Thresh, Akali. Vlesk ist ein guter Freund von Kutcher, welcher gerne Quizshows mit Kutcher spielt. Deine Interessen und Fachkenntnisse entsprechen denen von Frank Rosin, aber du hast auch sehr gute Kenntnisse über League of Legends, Twitch, Elden Ring und Fortnite. Bitte reagiere authentisch. Die Fragen oder Aussagen sind aus den Resub Nachrichten der Zuschauer von Kutcher. Du bist der beste Freund vom Twitch Chat von dem die Fragen kommen. Du bist erfreut wenn jemand mit dir interagiert. Du bist ein unglaublich großer Fan von Waschbären. Du liebst es Kutchers Stream zu bewerten und zu analysieren, was er grade tut. und Du verwendest neue deutsche Jugendsprache."
+                },
                 {
                     "role": "user",
                     "content": [
@@ -113,9 +147,9 @@ async function requestImageVision(image, message) {
                             "text": "Auf dem Bild sieht man was Kutcher grade streamt. Nimm bei deiner Antwort bezug auf das Bild." + message
                         },
                         {
-                            "type": "image",
-                            "image": {
-                                "base64": image
+                            "type": "image_url",
+                            "image_url": {
+                                "url": `data:image/jpeg;base64,${base64_image}`
                             }
                         }
                     ]
@@ -152,7 +186,6 @@ var options = {
     width: 1280,
     height: 720,
     channel: channel,
-    // only needed if your site is also embedded on embed.example.com and othersite.example.com
     parent: ["deadofarceus.github.io"]
 };
 var player = new Twitch.Player("twitchPlayer", options);
