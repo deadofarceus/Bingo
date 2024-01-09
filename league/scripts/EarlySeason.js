9ffec6d69ba892b66bce1e524303796bb3d53d1d
deadofarceus <nikilanger@yahoo.de>
2024-01-09 18:31:15 +0100

    feat: style adjustments

--- league/scripts/EarlySeason.js
+++ league/scripts/EarlySeason.js
@@ -32,7 +32,7 @@ var socket;
 function connectWebSocket() {
     socket = new WebSocket(`wss://modserver-dedo.glitch.me?id=${id}`);
     // socket = new WebSocket(`ws://localhost:8080?id=${id}&type=${type}&channel=${channel}`);
-    
+
     setInterval(ping, 60000);
 
     socket.onopen = function () {
@@ -59,7 +59,7 @@ function connectWebSocket() {
         if (message === "pong") {
             return;
         }
-        
+
         const data = JSON.parse(message);
         const accounts = data.accounts;
 
@@ -68,8 +68,12 @@ function connectWebSocket() {
 
         var V9ELo = 0;
         var NNOELo = 0;
+        var besterSpieler = undefined;
 
         accounts.forEach(player => {
+            if (!besterSpieler || besterSpieler.combinedLP < player.combinedLP) {
+                besterSpieler = player;
+            }
             if (player.name === "Sola" || player.name === "Kutcher") {
                 V9ELo += player.combinedLP
                 generatePlayer(player.name, player.tier, player.rank, player.leaguePoints, "V9");
@@ -81,6 +85,14 @@ function connectWebSocket() {
 
         setELO("V9", V9ELo);
         setELO("NNO", NNOELo);
+
+        if (V9ELo > NNOELo) {
+            betterTeam("V9");
+        } else if (V9ELo < NNOELo) {
+            betterTeam("NNO");
+        }
+
+        coronation(besterSpieler.name);
     };
 }
 
@@ -90,6 +102,24 @@ function ping() {
 
 // Initialer Verbindungsaufbau
 connectWebSocket();
+
+function coronation(name) {
+    var existingImg = document.getElementById(name + "img");
+    var overlayImg = document.createElement('img');
+    overlayImg.src = rPath + "crown.png";
+    overlayImg.alt = 'Overlay Image';
+    overlayImg.style.position = 'absolute';
+    overlayImg.style.bottom = '0';
+    overlayImg.style.left = '0';
+    overlayImg.style.width = "70px"
+    overlayImg.style.height = "70px"
+    existingImg.appendChild(overlayImg);
+}
+
+function betterTeam(team) {
+    const eloP = document.getElementById(team + "ELO");
+    eloP.style.border = "2px solid rgba(0, 250, 0, 0.4)"
+}
 
 function setELO(team, LP) {
     var eloP = document.getElementById(team + "ELO");
@@ -101,9 +131,15 @@ const rPath = "../ressources/";
 function generatePlayer(playerName, eloSymbolSrc, rank, lpValue, team) {
     var playerDiv = document.createElement('div');
 
+    var imageDiv = document.createElement('div');
+    imageDiv.id = playerName + "img";
+    imageDiv.classList.add("profileImgDiv");
+
     var img1 = document.createElement('img');
+    img1.classList.add("profileImg");
     img1.src = rPath + playerName + ".png";
     img1.alt = '';
+    imageDiv.appendChild(img1);
 
     var pText = document.createElement('p');
     pText.textContent = playerName;
@@ -116,12 +152,16 @@ function generatePlayer(playerName, eloSymbolSrc, rank, lpValue, team) {
     eloImg.alt = '';
 
     var lpP = document.createElement('p');
-    lpP.textContent = rank + " " + lpValue + ' LP';
+    if (eloSymbolSrc !== "EMERALD" && eloSymbolSrc !== "DIAMOND") {
+        lpP.textContent = lpValue + ' LP';
+    } else {
+        lpP.textContent = rank + " " + lpValue + ' LP';
+    }
 
     eloDiv.appendChild(eloImg);
     eloDiv.appendChild(lpP);
 
-    playerDiv.appendChild(img1);
+    playerDiv.appendChild(imageDiv);
     playerDiv.appendChild(pText);
     playerDiv.appendChild(eloDiv);
 
