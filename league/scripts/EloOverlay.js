b7d67b4ecde47b8d7f3034488820b72a7d1fbe38
deadofarceus <nikilanger@yahoo.de>
2024-01-29 16:10:09 +0100

    feat: add today LP gains

--- league/scripts/EloOverlay.js
+++ league/scripts/EloOverlay.js
@@ -1,46 +1,42 @@
 class Account {
-    summonerId;
-    name;
-    hashtag;
-    accountId;
-    puuid;
-    tier;
-    rank;
-    leaguePoints;
-    combinedLP;
-    wins;
-    loses;
-    hotstreak;
-    lastThree;
+  summonerId;
+  name;
+  hashtag;
+  accountId;
+  puuid;
+  tier;
+  rank;
+  leaguePoints;
+  combinedLP;
+  wins;
+  loses;
+  hotstreak;
+  lastThree;
 
-    constructor(
-        summonerId,
-        name,
-        hashtag,
-    ) {
-        this.summonerId = summonerId;
-        this.name = name;
-        this.hashtag = hashtag;
-    }
+  constructor(summonerId, name, hashtag) {
+    this.summonerId = summonerId;
+    this.name = name;
+    this.hashtag = hashtag;
+  }
 }
 
 class ModEvent {
-    type;
-    data;
-    constructor(type, data) {
-        this.type = type;
-        this.data = data;
-    }
-    tostring() {
-        return JSON.stringify(this);
-    }
+  type;
+  data;
+  constructor(type, data) {
+    this.type = type;
+    this.data = data;
+  }
+  tostring() {
+    return JSON.stringify(this);
+  }
 }
 
 var url = new URL(window.location.href);
 var params = new URLSearchParams(url.search);
-const summonerName = params.get('name');
-const tag = params.get('tag');
-const key = params.get('key');
+const summonerName = params.get("name");
+const tag = params.get("tag");
+const key = params.get("key");
 
 var socket;
 const endTime = 1706137199000;
@@ -51,48 +47,64 @@ var connectAgain = true;
 var savedAccount = new Account("", summonerName, tag);
 
 function connectWebSocket() {
-    socket = new WebSocket(`wss://modserver-dedo.glitch.me?name=${summonerName}&tag=${tag}`);
-    // socket = new WebSocket(`ws://localhost:8080?name=${summonerName}&tag=${tag}`);
+  socket = new WebSocket(
+    `wss://modserver-dedo.glitch.me?name=${summonerName}&tag=${tag}`
+  );
+  // socket = new WebSocket(`ws://localhost:8080?name=${summonerName}&tag=${tag}`);
 
-    pingInterval = setInterval(ping, 60000);
+  pingInterval = setInterval(ping, 60000);
 
-    socket.onopen = function () {
-        console.log('WebSocket-Verbindung hergestellt.');
-        const modEvent = new ModEvent("league/listenAccount", { summonerName: summonerName, tag: tag, key: key });
-        socket.send(JSON.stringify(modEvent));
-    };
+  socket.onopen = function () {
+    console.log("WebSocket-Verbindung hergestellt.");
+    const modEvent = new ModEvent("league/listenAccount", {
+      summonerName: summonerName,
+      tag: tag,
+      key: key,
+    });
+    socket.send(JSON.stringify(modEvent));
+  };
 
-    socket.onclose = function () {
-        console.log('WebSocket-Verbindung geschlossen. Versuche erneut zu verbinden...');
-        clearInterval(pingInterval);
-        connectWebSocket(); // Verbindung nach 2 Sekunden erneut aufbauen
-    };
+  socket.onclose = function () {
+    console.log(
+      "WebSocket-Verbindung geschlossen. Versuche erneut zu verbinden..."
+    );
+    clearInterval(pingInterval);
+    connectWebSocket(); // Verbindung nach 2 Sekunden erneut aufbauen
+  };
 
-    socket.onerror = function (error) {
-        console.error('WebSocket-Fehler aufgetreten: ', error);
-    };
+  socket.onerror = function (error) {
+    console.error("WebSocket-Fehler aufgetreten: ", error);
+  };
 
-    socket.onmessage = function (event) {
-        const message = event.data;
+  socket.onmessage = function (event) {
+    const message = event.data;
 
-        if (message === "pong") {
-            return;
-        }
+    if (message === "pong") {
+      return;
+    }
 
-        const data = JSON.parse(message);
-        const account = data.accounts[0];
-        console.log(account);
-        savedAccount = account;
+    const data = JSON.parse(message);
+    const account = data.accounts[0];
+    console.log(account);
+    savedAccount = account;
 
-        document.getElementById("Player").innerHTML = '';
+    document.getElementById("Player").innerHTML = "";
 
-        generatePlayer(account.name, account.tier, account.rank, account.leaguePoints, account.lastThree);
+    const lpDiff = account.combinedLP - account.lpStart;
 
-    };
+    generatePlayer(
+      account.name,
+      account.tier,
+      account.rank,
+      account.leaguePoints,
+      account.lastThree,
+      lpDiff
+    );
+  };
 }
 
 function ping() {
-    socket.send("ping");
+  socket.send("ping");
 }
 
 // Initialer Verbindungsaufbau
@@ -100,63 +112,95 @@ connectWebSocket();
 
 const rPath = "../ressources/";
 
-function generatePlayer(playerName, eloSymbolSrc, rank, lpValue, matches) {
-    var playerDiv = document.createElement('div');
+function generatePlayer(
+  playerName,
+  eloSymbolSrc,
+  rank,
+  lpValue,
+  matches,
+  lpDiff
+) {
+  var playerDiv = document.createElement("div");
 
-    var eloDiv = document.createElement('div');
-    eloDiv.className = 'ELO';
+  var playerInfoDiv = document.createElement("div");
+  playerInfoDiv.id = "playerInfo";
 
-    var eloImg = document.createElement('img');
-    eloImg.src = rPath + eloSymbolSrc + ".png";
-    eloImg.alt = '';
-    eloImg.classList.add("eloimg");
+  var eloDiv = document.createElement("div");
+  var lpDiffDiv = document.createElement("div");
+  eloDiv.className = "ELO";
+  lpDiffDiv.className = "ELO";
 
-    var lpP = document.createElement('p');
-    if (eloSymbolSrc !== "EMERALD" && eloSymbolSrc !== "DIAMOND") {
-        lpP.textContent = lpValue + ' LP';
-    } else {
-        lpP.textContent = rank + " " + lpValue + ' LP';
-    }
+  var eloImg = document.createElement("img");
+  eloImg.src = rPath + eloSymbolSrc + ".png";
+  eloImg.alt = "";
+  eloImg.classList.add("eloimg");
 
-    eloDiv.appendChild(eloImg);
-    eloDiv.appendChild(lpP);
+  var lpP = document.createElement("p");
+  if (eloSymbolSrc !== "EMERALD" && eloSymbolSrc !== "DIAMOND") {
+    lpP.textContent = lpValue + " LP";
+  } else {
+    lpP.textContent = rank + " " + lpValue + " LP";
+  }
 
-    playerDiv.appendChild(eloDiv);
+  var lpDiffP = document.createElement("p");
+  lpDiffP.classList.add("lpDiff");
+  if (lpDiff >= 0) {
+    lpDiffP.innerHTML = `+${lpDiff} LP &uarr;`;
+    lpDiffP.style.color = "rgb(33, 209, 6)";
+  } else {
+    lpDiffP.innerHTML = `${lpDiff} LP &darr;`;
+    lpDiffP.style.color = "rgb(255, 28, 24)";
+  }
 
-    //create foreach match icon and arrow down red or green up `https://ddragon.leagueoflegends.com/cdn/14.2.1/img/champion/${championName}.png`
-    var matchesDiv = document.createElement('div');
-    matchesDiv.classList.add("row");
-    matches.forEach((match, index) => {
-        var matchDiv = document.createElement('div');
-        var champIMG = document.createElement('img');
-        matchDiv.id = index + "img";
-        matchDiv.classList.add("imgdiv");
-        champIMG.src = `https://ddragon.leagueoflegends.com/cdn/14.2.1/img/champion/${match.championName}.png`;
-        champIMG.alt = '';
-        champIMG.classList.add("profileImg");
-        champIMG.style.height = `${40-(matches.length-1-index)}px`;
-        matchDiv.appendChild(champIMG);
-        matchesDiv.appendChild(matchDiv);
-    });
+  var today = document.createElement("p");
+  today.innerHTML = "Heute:";
+  today.classList.add("lpDiff");
 
-    playerDiv.appendChild(matchesDiv);
+  lpDiffDiv.appendChild(today);
+  lpDiffDiv.appendChild(lpDiffP);
 
-    var parentplayerDiv = document.getElementById("Player");
-    parentplayerDiv.appendChild(playerDiv);
+  eloDiv.appendChild(eloImg);
+  eloDiv.appendChild(lpP);
 
-    matches.forEach((match, index) => {
-        addArrow(index, match.win, matches.length-1-index);
-    });
+  playerInfoDiv.appendChild(eloDiv);
+  playerInfoDiv.appendChild(lpDiffDiv);
+
+  playerDiv.appendChild(playerInfoDiv);
+
+  //create foreach match icon and arrow down red or green up `https://ddragon.leagueoflegends.com/cdn/14.2.1/img/champion/${championName}.png`
+  var matchesDiv = document.createElement("div");
+  matchesDiv.classList.add("row");
+  matches.forEach((match, index) => {
+    var matchDiv = document.createElement("div");
+    var champIMG = document.createElement("img");
+    matchDiv.id = index + "img";
+    matchDiv.classList.add("imgdiv");
+    champIMG.src = `https://ddragon.leagueoflegends.com/cdn/14.2.1/img/champion/${match.championName}.png`;
+    champIMG.alt = "";
+    champIMG.classList.add("profileImg");
+    champIMG.style.height = `${40 - (matches.length - 1 - index)}px`;
+    matchDiv.appendChild(champIMG);
+    matchesDiv.appendChild(matchDiv);
+  });
+
+  playerDiv.appendChild(matchesDiv);
+
+  var parentplayerDiv = document.getElementById("Player");
+  parentplayerDiv.appendChild(playerDiv);
+
+  matches.forEach((match, index) => {
+    addArrow(index, match.win, matches.length - 1 - index);
+  });
 }
 
 function addArrow(name, direction, length) {
-    var existingImg = document.getElementById(name + "img");
-    var overlayImg = document.createElement('img');
-    overlayImg.src = rPath + direction + ".png";
-    overlayImg.alt = 'Overlay Image';
-    overlayImg.classList.add("overlayIMG");
-    overlayImg.style.height = `${40-(length)}px`;
-    existingImg.appendChild(overlayImg);
+  var existingImg = document.getElementById(name + "img");
+  var overlayImg = document.createElement("img");
+  overlayImg.src = rPath + direction + ".png";
+  overlayImg.alt = "Overlay Image";
+  overlayImg.classList.add("overlayIMG");
+  overlayImg.style.height = `${40 - length}px`;
+  existingImg.appendChild(overlayImg);
 }
 
 // Beispielaufruf der Funktion mit Dummy-Werten
